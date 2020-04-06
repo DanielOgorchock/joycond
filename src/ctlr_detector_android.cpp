bfb7c6798d033e5a37133d863b84e03263f52d61
Billy Laws <blaws05@gmail.com>
2020-04-06 22:10:41 +0100

    android: refactor controller detection
    Now correctly identifies bluetooth disconnect and misc code cleanups.

--- src/ctlr_detector_android.cpp
+++ src/ctlr_detector_android.cpp
@@ -16,6 +16,55 @@
 #include <libevdev/libevdev.h>
 
 //private
+bool ctlr_detector_android::check_ctlr_attributes(std::string devpath)
+{
+    struct libevdev *evdev;
+
+    // Open device to confirm the vendor and product id, not given in uevent
+    int fd = open(devpath.c_str(), O_RDWR | O_NONBLOCK);
+    if (fd < 0) {
+        std::cerr << "Failed to open " << devpath << " ; errno=" << errno << std::endl;
+        return false;
+    }
+
+    if (libevdev_new_from_fd(fd, &evdev)) {
+        std::cerr << "Failed to create evdev from fd\n";
+        close(fd);
+        return false;
+    }
+
+    int pid = libevdev_get_id_product(evdev);
+    int vid = libevdev_get_id_vendor(evdev);
+
+    libevdev_free(evdev);
+    close(fd);
+
+    std::cout << "Input device connected vid: 0x" << std::hex << vid << " pid: 0x" << std::hex << pid << std::endl;
+
+    if (vid != 0x57e)
+        return false;
+
+    if (pid != 0x2009 && pid != 0x2007 && pid != 0x2006)
+        return false;
+
+    return true;
+}
+
+//private
+void ctlr_detector_android::scan_removed_ctlrs() {
+    // Scan all controllers we think are connected to double check they actually are, removing if needed
+
+    for (auto ctlr : ctlr_dev_map) {
+        if (access(ctlr.second.c_str(), F_OK) != -1) {
+            // Controller has been disconnected, it's event file is missing
+            ctlr_manager.remove_ctlr(ctlr.first);
+            ctlr_dev_map.erase(ctlr.first);
+            return;
+        }
+    }
+}
+
+//private
 void ctlr_detector_android::epoll_event_callback(int event_fd)
 {
     char buf[8192];
@@ -27,15 +76,19 @@ void ctlr_detector_android::epoll_event_callback(int event_fd)
     event_msg = { &event_sockaddr, sizeof(event_sockaddr), &event_iovec, 1, NULL, 0, 0 };
     event_len = recvmsg(event_fd, &event_msg, 0);
    
-    std::string devpath, devname, key, val;
+    std::string devpath, devnode, key, val;
 
-    bool action = false; // Removed
+    bool action = false;
     bool correct = false;
+
+    // On Android Bluetooth disconnect only results in a hidraw disconnect event so use that
+    // to remove controllers.
+    bool hidraw_remove = false;
 
     while (pos < event_len) {
         std::istringstream iss(&buf[pos]);
-        while(std::getline(std::getline(iss, key, '=') >> std::ws, val)) {
-            if ((key.find("ACTION") != std::string::npos)) { //
+        while (std::getline(std::getline(iss, key, '=') >> std::ws, val)) {
+            if ((key.find("ACTION") != std::string::npos)) {
                 if (val == "add") {
                     correct = true;
                     action = true;
@@ -45,62 +98,47 @@ void ctlr_detector_android::epoll_event_callback(int event_fd)
                 }
             }
 
-            if ((key == "SUBSYSTEM") && (val == "input"))
-                correct = true && correct;
+            if (key == "SUBSYSTEM") {
+                correct = ((val == "input") || (val == "hidraw")) && correct;
+                hidraw_remove = (val == "hidraw");
+            }
 
             if (key == "DEVPATH")
                 devpath = val;
 
             if (key == "DEVNAME" && val.find("/dev/") == std::string::npos)
-                devname = "/dev/" + val;
+                devnode = "/dev/" + val;
         }
         pos += strlen(&buf[pos]) + 1;
     }
 
     if (!correct)
         return;
-    
+ 
     // Only accept event* devices and complete requests
-    if (devpath.empty() || devname.empty() || (devname.find("event") == std::string::npos))
+    if (devpath.empty() || devnode.empty() || ((devnode.find("event") == std::string::npos) && (devnode.find("hid") == std::string::npos)))
         return;
+
+    devpath = "/class/input/" + std::string(basename(devnode.c_str())) + "/device/device";
 
     // Sleep a bit to let driver load
     usleep(100000);
 
-    // We obviously can't read the sysfs of a removed device
     if (!action) {
-        ctlr_manager.remove_ctlr(devpath);
+        if (hidraw_remove) {
+            scan_removed_ctlrs();
+        } else {
+            ctlr_dev_map.erase(devnode);
+            ctlr_manager.remove_ctlr(devpath);
+        }
+
         return;
     }
 
-    struct libevdev *evdev;
-
-    // Open device to confirm the vendor and product id, not given in uevent
-    int fd = open(devname.c_str(), O_RDWR | O_NONBLOCK);
-    if (fd < 0) {
-        std::cerr << "Failed to open " << devname << " ; errno=" << errno << std::endl;
-        return;
+    if (check_ctlr_attributes(devnode)) {
+        ctlr_manager.add_ctlr(devpath, devnode);
+        ctlr_dev_map.insert({devpath, devnode});
     }
-    if (libevdev_new_from_fd(fd, &evdev)) {
-        std::cerr << "Failed to create evdev from fd\n";
-        close(fd);
-        return;
-    }
-
-    int pid = libevdev_get_id_product(evdev);
-    int vid = libevdev_get_id_vendor(evdev);
-
-    libevdev_free(evdev);
-    close(fd);
-    
-    std::cout << "Input device connected vid: 0x" << std::hex << vid <<  " pid: 0x" << std::hex << pid << std::endl;
-    if (vid != 0x57e)
-        return;
-
-    if (pid != 0x2009 && pid != 0x2007 && pid != 0x2006)
-        return;
-
-    ctlr_manager.add_ctlr(devpath, devname);
 }
 
 //public
@@ -122,51 +160,31 @@ ctlr_detector_android::ctlr_detector_android(ctlr_mgr& ctlr_manager, epoll_mgr& 
             continue;
 
         event_path = "/dev/input/" + std::string(event_dirent->d_name);
-        sysfs_event_path = "/sys/class/input/" + std::string(event_dirent->d_name) + "/device";
+        sysfs_event_path = "/class/input/" + std::string(event_dirent->d_name) + "/device/device";
   
-        // Open device to confirm the vendor and product id
-        int fd = open(event_path.c_str(), O_RDWR | O_NONBLOCK);
-        if (fd < 0) {
-            std::cerr << "Failed to open " << event_path << "; errno=" << errno << std::endl;
-            continue;
+        if (check_ctlr_attributes(event_path)) {
+            ctlr_manager.add_ctlr(sysfs_event_path, event_path);
+            ctlr_dev_map.insert({sysfs_event_path, event_path});
         }
-
-        struct libevdev *evdev;
-        if (libevdev_new_from_fd(fd, &evdev)) {
-            std::cerr << "Failed to create evdev from fd\n";
-            close(fd);
-            continue;
-        }
-
-        int pid = libevdev_get_id_product(evdev);
-        int vid = libevdev_get_id_vendor(evdev);
-
-        libevdev_free(evdev);
-        close(fd);
-
-        std::cout << "Input device connected vid: 0x" << std::hex << vid <<  " pid: 0x" << std::hex << pid << std::endl;
-        if (vid != 0x57e)
-            continue;
-
-        if (pid != 0x2009 && pid != 0x2007 && pid != 0x2006)
-            continue;
-
-        ctlr_manager.add_ctlr(sysfs_event_path, event_path);
     }
 
-    // Open hotplug event netlink socket
+    // Open netlink socket
     memset(&uevent_socket,0,sizeof(struct sockaddr_nl));
     uevent_socket.nl_family = AF_NETLINK;
     uevent_socket.nl_pid = getpid();
     uevent_socket.nl_groups = -1;
     uevent_pollfd.events = POLLIN;
     uevent_pollfd.fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
-    if (uevent_pollfd.fd==-1)
-        std::cout << "Unable to create polling fd: EPERM" << std::endl;
+    if (uevent_pollfd.fd == -1) {
+        std::cerr << "Unable to create polling fd:" << std::endl;
+        return;
+    }
 
     // Listen to netlink socket
-    if (bind(uevent_pollfd.fd, (struct sockaddr*)&uevent_socket, sizeof(struct sockaddr_nl)))
-        std::cout << "Unable to create bind poll fd" << std::endl;
+    if (bind(uevent_pollfd.fd, (struct sockaddr*)&uevent_socket, sizeof(struct sockaddr_nl))) {
+        std::cerr << "Unable to create bind poll fd" << std::endl;
+        return;
+    }
 
     subscriber = std::make_shared<epoll_subscriber>(std::vector({uevent_pollfd.fd}),
                                                     [=](int event_fd){epoll_event_callback(event_fd);});
